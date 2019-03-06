bf7d95857941c9cbb6eb70e56d52994220f0febc
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2019-03-06 15:54:37 -0300

    Updates

--- static/js/6.c50d9cb7.js
+++ static/js/6.c50d9cb7.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"./.docz/app/imports.js":function(n,t,o){"use strict";o.r(t),o.d(t,"imports",function(){return r});var r={"src/index.mdx":function(){return o.e(4).then(o.bind(null,"./src/index.mdx"))},"src/components/Button/button.mdx":function(){return o.e(2).then(o.bind(null,"./src/components/Button/button.mdx"))},"src/components/Typography/typography.mdx":function(){return o.e(3).then(o.bind(null,"./src/components/Typography/typography.mdx"))}}}},0,[2,3,4]]);
-//# sourceMappingURL=6.a32dbdcba5ecd66375e3.js.map
+//# sourceMappingURL=6.b9b5e1ac9c0e5e5bd503.js.map
